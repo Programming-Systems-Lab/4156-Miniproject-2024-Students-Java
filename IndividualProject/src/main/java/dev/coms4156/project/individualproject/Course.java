da12d6a2bd7d2e56ceaeddea3d9ad532b8b93f90
CarrilloMario831 <carrillomario831@gmail.com>
2024-09-08 19:58:55 -0400

    Course.java -> Added javadoc comments for methods/functions. Fixed incorrect dropStudent() and enrollStudent() functionality. Fixed getCourseLocation() and getInstructorName() to return the correct courseLocation and instructorName.

--- IndividualProject/src/main/java/dev/coms4156/project/individualproject/Course.java
+++ IndividualProject/src/main/java/dev/coms4156/project/individualproject/Course.java
@@ -26,7 +26,10 @@ public class Course implements Serializable {
    * @return true if the student is successfully enrolled, false otherwise.
    */
   public boolean enrollStudent() {
-   enrolledStudentCount++;
+    if (!isCourseFull()) {
+      enrolledStudentCount++;
+      return true;
+    }
     return false;
   }
 
@@ -36,53 +39,95 @@ public class Course implements Serializable {
    * @return true if the student is successfully dropped, false otherwise.
    */
   public boolean dropStudent() {
-    enrolledStudentCount--;
+    if (enrolledStudentCount > 0) {
+      enrolledStudentCount--;
+      return true;
+    }
     return false;
   }
 
-
+  /**
+   * Gets the course location.
+   *
+   * @return The location of the course as a string.
+   */
   public String getCourseLocation() {
-    return this.instructorName;
-  }
-
-
-  public String getInstructorName() {
     return this.courseLocation;
   }
 
+  /**
+   * Gets the instructor name.
+   *
+   * @return The name of the course instructor as a string.
+   */
+  public String getInstructorName() {
+    return this.instructorName;
+  }
 
+  /**
+   * Gets the course time slot.
+   *
+   * @return The course time slot as a string.
+   */
   public String getCourseTimeSlot() {
     return this.courseTimeSlot;
   }
 
-
+  /**
+   * A string representation of the course details.
+   * Such as instructor name, course location, and course time slot.
+   *
+   * @return A string representation of the course.
+   */
   public String toString() {
-    return "\nInstructor: " + instructorName +  "; Location: "  + courseLocation +  "; Time: " + courseTimeSlot;
+    return "\nInstructor: " + instructorName +
+            "; Location: "  + courseLocation +
+            "; Time: " + courseTimeSlot;
   }
 
-
+  /**
+   * Reassigns the instructor for the course.
+   *
+   * @param newInstructorName The name of the new instructor.
+   */
   public void reassignInstructor(String newInstructorName) {
     this.instructorName = newInstructorName;
   }
 
-
+  /**
+   * Reassigns the location for the course.
+   *
+   * @param newLocation The new location of the course.
+   */
   public void reassignLocation(String newLocation) {
     this.courseLocation = newLocation;
   }
 
-
+  /**
+   * Reassigns the time for the course.
+   *
+   * @param newTime The new time of the course.
+   */
   public void reassignTime(String newTime) {
     this.courseTimeSlot = newTime;
   }
 
-
+  /**
+   * Sets the number of enrolled students in the course.
+   *
+   * @param count The new count of students.
+   */
   public void setEnrolledStudentCount(int count) {
     this.enrolledStudentCount = count;
   }
 
-
+  /**
+   * Checks if the course is full.
+   *
+   * @return true if the course is full, false otherwise.
+   */
   public boolean isCourseFull() {
-    return enrollmentCapacity > enrolledStudentCount;
+    return enrolledStudentCount >= enrollmentCapacity;
   }
 
   @Serial
