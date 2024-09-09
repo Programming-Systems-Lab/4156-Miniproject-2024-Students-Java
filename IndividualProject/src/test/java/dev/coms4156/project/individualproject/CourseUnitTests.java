b00584898383173d5358254c58c0066e38ddc8ca
WillMa17 <68440147+WillMa17@users.noreply.github.com>
2024-09-09 16:31:24 -0400

    Finished modification for code style (#1)

--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
@@ -1,11 +1,16 @@
 package dev.coms4156.project.individualproject;
 
-import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * This class contains unit tests for the course class.
+ */
 @SpringBootTest
 @ContextConfiguration
 public class CourseUnitTests {
