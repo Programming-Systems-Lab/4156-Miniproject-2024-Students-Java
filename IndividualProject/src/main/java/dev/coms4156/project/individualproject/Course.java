c6f48ee5a5d448185d440ae1f4f45a871f064259
Madhura Chatterjee <madhurachatterjee@Madhuras-Laptop.lan>
2024-09-08 20:15:22 -0400

    Cleaning up code

--- IndividualProject/src/main/java/dev/coms4156/project/individualproject/Course.java
+++ IndividualProject/src/main/java/dev/coms4156/project/individualproject/Course.java
@@ -1,7 +1,11 @@
 package dev.coms4156.project.individualproject;
 
-import java.io.*;
+import java.io.Serial;
+import java.io.Serializable;
 
+/**
+* Defines courses.
+*/
 public class Course implements Serializable {
 
   /**
@@ -20,21 +24,21 @@ public class Course implements Serializable {
     this.enrolledStudentCount = 500;
   }
 
- /**
-   * Enrolls a student in the course if there is space available.
-   *
-   * @return true if the student is successfully enrolled, false otherwise.
-   */
+  /**
+  * Enrolls a student in the course if there is space available.
+  *
+  * @return true if the student is successfully enrolled, false otherwise.
+  */
   public boolean enrollStudent() {
-   enrolledStudentCount++;
+    enrolledStudentCount++;
     return false;
   }
 
- /**
-   * Drops a student from the course if a student is enrolled.
-   *
-   * @return true if the student is successfully dropped, false otherwise.
-   */
+  /**
+  * Drops a student from the course if a student is enrolled.
+  *
+  * @return true if the student is successfully dropped, false otherwise.
+  */
   public boolean dropStudent() {
     enrolledStudentCount--;
     return false;
@@ -57,7 +61,8 @@ public class Course implements Serializable {
 
 
   public String toString() {
-    return "\nInstructor: " + instructorName +  "; Location: "  + courseLocation +  "; Time: " + courseTimeSlot;
+    return "\nInstructor: " + instructorName +  "; Location: "
+      + courseLocation +  "; Time: " + courseTimeSlot;
   }
 
 
