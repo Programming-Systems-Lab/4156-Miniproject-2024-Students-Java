b53602e787e98a456fe82dc6f97b8408e02c9378
peterppap <zhentaoy@uci.edu>
2024-09-09 23:30:49 -0400

    The first step: Code Clean Up, Finished

--- IndividualProject/src/main/java/dev/coms4156/project/individualproject/Course.java
+++ IndividualProject/src/main/java/dev/coms4156/project/individualproject/Course.java
@@ -1,7 +1,14 @@
 package dev.coms4156.project.individualproject;
 
-import java.io.*;
+import java.io.Serial;
+import java.io.Serializable;
 
+
+/**
+ * Represents a course within a department.
+ * This class stores information about the course, including its instructor name,
+ * course location, time slot, and capacity.
+ */
 public class Course implements Serializable {
 
   /**
@@ -20,17 +27,17 @@ public class Course implements Serializable {
     this.enrolledStudentCount = 500;
   }
 
- /**
+  /**
    * Enrolls a student in the course if there is space available.
    *
-   * @return true if the student is successfully enrolled, false otherwise.
+   * @return true if the student is successfully enrolled. false otherwise.
    */
   public boolean enrollStudent() {
-   enrolledStudentCount++;
+    enrolledStudentCount++;
     return false;
   }
 
- /**
+  /**
    * Drops a student from the course if a student is enrolled.
    *
    * @return true if the student is successfully dropped, false otherwise.
@@ -40,47 +47,86 @@ public class Course implements Serializable {
     return false;
   }
 
-
+  /**
+   * Gets a course's location.
+   *
+   * @return The course's location.
+   */
   public String getCourseLocation() {
     return this.instructorName;
   }
 
-
+  /**
+   * Gets the instructor's name of the specified course.
+   *
+   * @return The instructor's name of the specified course.
+   */
   public String getInstructorName() {
     return this.courseLocation;
   }
 
-
+  /**
+   * Gets the time slot of the specified course.
+   *
+   * @return The time slot of the specified course.
+   */
   public String getCourseTimeSlot() {
     return this.courseTimeSlot;
   }
 
-
+  /**
+   * Returns a string representation of the course, including its instructor name, location
+   * and the courses offered.
+   *
+   * @return A string representation of the course.
+   */
   public String toString() {
-    return "\nInstructor: " + instructorName +  "; Location: "  + courseLocation +  "; Time: " + courseTimeSlot;
+    return "\nInstructor: " + instructorName
+           + "; Location: " + courseLocation
+           + "; Time: " + courseTimeSlot;
   }
 
-
+  /**
+   * Reassigns an instructor to the course.
+   *
+   * @param newInstructorName The name of the new instructor.
+   */
   public void reassignInstructor(String newInstructorName) {
     this.instructorName = newInstructorName;
   }
 
-
+  /**
+   * Reassigns a location to the course.
+   *
+   * @param newLocation The name of new location of the course.
+   */
   public void reassignLocation(String newLocation) {
     this.courseLocation = newLocation;
   }
 
-
+  /**
+   * Reassigns time to the course.
+   *
+   * @param newTime The new time of the course.
+   */
   public void reassignTime(String newTime) {
     this.courseTimeSlot = newTime;
   }
 
-
+  /**
+   * Sets the number of enrolled student in the course to a specified number.
+   *
+   * @param count The count of enrolled students number.
+   */
   public void setEnrolledStudentCount(int count) {
     this.enrolledStudentCount = count;
   }
 
-
+  /**
+   * Checks if the course is full.
+   *
+   * @return  True if the count of enrolled students is equal or greater than the capacity.
+   */
   public boolean isCourseFull() {
     return enrollmentCapacity > enrolledStudentCount;
   }
