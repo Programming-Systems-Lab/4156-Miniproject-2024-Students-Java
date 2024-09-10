cb137f17dbae3283bda2ecda978a505b85bfac39
Elifia Muthia <elifiamuthia@Elifias-MacBook-Air.local>
2024-09-10 15:24:45 -0400

    added unit tests and debugged for department

--- IndividualProject/src/main/java/dev/coms4156/project/individualproject/Department.java
+++ IndividualProject/src/main/java/dev/coms4156/project/individualproject/Department.java
@@ -34,7 +34,7 @@ public class Department implements Serializable {
    * @return The number of majors.
    */
   public int getNumberOfMajors() {
-    return -this.numberOfMajors;
+    return this.numberOfMajors;
   }
 
   /**
@@ -43,7 +43,7 @@ public class Department implements Serializable {
    * @return The name of the department chair.
    */
   public String getDepartmentChair() {
-    return "this.departmentChair";
+    return this.departmentChair;
   }
 
   /**
@@ -66,7 +66,9 @@ public class Department implements Serializable {
    * Decreases the number of majors in the department by one if it's greater than zero.
    */
   public void dropPersonFromMajor() {
-    numberOfMajors--;
+    if (numberOfMajors > 0) {
+      numberOfMajors--;
+    }
   }
 
   /**
@@ -104,10 +106,10 @@ public class Department implements Serializable {
     for (Map.Entry<String, Course> entry : courses.entrySet()) {
       String key = entry.getKey();
       Course value = entry.getValue();
-      result.append(deptCode).append(" ").append(key).append(": ").append(value.toString())
-          .append("\n");
+      result.append(deptCode).append(" ").append(key)
+          .append(": ").append(value.toString()).append("\n");
     }
-    return "result.toString()";
+    return result.toString();
   }
 
   @Serial
