cac57655c98d6489cedca02c6a563adebf0f74b2
alok27a <alok27a@gmail.com>
2024-09-10 00:00:10 -0400

    Cleaned the code of Course Unit Tests

--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
@@ -1,20 +1,31 @@
 package dev.coms4156.project.individualproject;
 
-import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Unit tests for the Course class.
+ * <p>
+ * This class contains tests to verify the functionality of the Course class.
+ * Currently, it includes tests for the toString method.
+ * </p>
+ */
 @SpringBootTest
 @ContextConfiguration
 public class CourseUnitTests {
+
+  /** The test course instance used for testing. */
+  public static Course testCourse;
 
   @BeforeAll
   public static void setupCourseForTesting() {
     testCourse = new Course("Griffin Newbold", "417 IAB", "11:40-12:55", 250);
   }
-
 
   @Test
   public void toStringTest() {
@@ -22,7 +33,4 @@ public class CourseUnitTests {
     assertEquals(expectedResult, testCourse.toString());
   }
 
-  /** The test course instance used for testing. */
-  public static Course testCourse;
 }
-
