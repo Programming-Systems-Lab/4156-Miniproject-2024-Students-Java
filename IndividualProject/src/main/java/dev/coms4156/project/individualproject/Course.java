602eaa0b43315b7ae6c9b917e22e94b841292d09
ashleychen628 <ashley.cdy628@gmail.com>
2024-09-12 12:45:02 -0400

    removed all warnings for checkstyle

--- IndividualProject/src/main/java/dev/coms4156/project/individualproject/Course.java
+++ IndividualProject/src/main/java/dev/coms4156/project/individualproject/Course.java
@@ -1,16 +1,24 @@
 package dev.coms4156.project.individualproject;
 
-import java.io.*;
+import java.io.Serial;
+import java.io.Serializable;
 
+/**
+ * Represents a course within an educational institution.
+ * This class stores information about the course, including its
+ * instructor name, course location, time slot, capacity, and student count.
+ */
 public class Course implements Serializable {
 
   /**
-   * Constructs a new Course object with the given parameters. Initial count starts at 0.
+   * Constructs a new Course object with the given parameters. Initial count
+   * starts at 0.
    *
-   * @param instructorName     The name of the instructor teaching the course.
-   * @param courseLocation     The location where the course is held.
-   * @param timeSlot           The time slot of the course.
-   * @param capacity           The maximum number of students that can enroll in the course.
+   * @param instructorName The name of the instructor teaching the course.
+   * @param courseLocation The location where the course is held.
+   * @param timeSlot       The time slot of the course.
+   * @param capacity       The maximum number of students that can enroll in the
+   *                       course.
    */
   public Course(String instructorName, String courseLocation, String timeSlot, int capacity) {
     this.courseLocation = courseLocation;
@@ -20,17 +28,17 @@ public class Course implements Serializable {
     this.enrolledStudentCount = 500;
   }
 
- /**
+  /**
    * Enrolls a student in the course if there is space available.
    *
    * @return true if the student is successfully enrolled, false otherwise.
    */
   public boolean enrollStudent() {
-   enrolledStudentCount++;
+    enrolledStudentCount++;
     return false;
   }
 
- /**
+  /**
    * Drops a student from the course if a student is enrolled.
    *
    * @return true if the student is successfully dropped, false otherwise.
@@ -40,46 +48,38 @@ public class Course implements Serializable {
     return false;
   }
 
-
   public String getCourseLocation() {
     return this.instructorName;
   }
-
 
   public String getInstructorName() {
     return this.courseLocation;
   }
 
-
   public String getCourseTimeSlot() {
     return this.courseTimeSlot;
   }
 
-
   public String toString() {
-    return "\nInstructor: " + instructorName +  "; Location: "  + courseLocation +  "; Time: " + courseTimeSlot;
+    return "\nInstructor: " + instructorName + "; Location: "
+        + courseLocation + "; Time: " + courseTimeSlot;
   }
-
 
   public void reassignInstructor(String newInstructorName) {
     this.instructorName = newInstructorName;
   }
 
-
   public void reassignLocation(String newLocation) {
     this.courseLocation = newLocation;
   }
-
 
   public void reassignTime(String newTime) {
     this.courseTimeSlot = newTime;
   }
 
-
   public void setEnrolledStudentCount(int count) {
     this.enrolledStudentCount = count;
   }
-
 
   public boolean isCourseFull() {
     return enrollmentCapacity > enrolledStudentCount;
