3104934660b5e09205fbc8f1a8d5a877d50ed19c
djuarez2652 <djuarez0440@gmail.com>
2024-09-13 12:51:51 -0400

    test: create new unit test file for RouteController.java

---
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/RouteControllerUnitTests.java
@@ -0,0 +1,19 @@
+package dev.coms4156.project.individualproject;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+/**
+ * This class contains all the unit tests for the RouteController class.
+ */
+public class RouteControllerUnitTests {
+
+  @BeforeAll
+  public static void setupRouteControllerForTesting() {
+
+  }
+}
