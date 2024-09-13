727962d3bf30d3d2e7556020a06ee3d459e96ba8
Bhavya <saiclubfairs@gmail.com>
2024-09-13 18:05:58 -0400

    style: fixed all formatting errors in CourseUnitTests, Included java doc comments and changed import statement location

--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
@@ -1,21 +1,31 @@
 package dev.coms4156.project.individualproject;
 
-import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertsEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Has unit tests for the Course class to check behavior.
+ */
 @SpringBootTest
 @ContextConfiguration
 public class CourseUnitTests {
 
+  /**
+   * Sets up an instance for Course for testing.
+   */
   @BeforeAll
   public static void setupCourseForTesting() {
     testCourse = new Course("Griffin Newbold", "417 IAB", "11:40-12:55", 250);
   }
 
-
+  /**
+   * Sets up a test for the ToString method.
+   */
   @Test
   public void toStringTest() {
     String expectedResult = "\nInstructor: Griffin Newbold; Location: 417 IAB; Time: 11:40-12:55";
