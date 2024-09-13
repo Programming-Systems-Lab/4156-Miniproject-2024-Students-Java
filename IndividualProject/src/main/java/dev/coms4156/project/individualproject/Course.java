ce6d2c2c2e251cde93e5515ee16f88e3c051ef81
Chunyu <chunyusui@outlook.com>
2024-09-13 15:42:15 -0400

    fix check-style warn

--- IndividualProject/src/main/java/dev/coms4156/project/individualproject/Course.java
+++ IndividualProject/src/main/java/dev/coms4156/project/individualproject/Course.java
@@ -1,16 +1,28 @@
 package dev.coms4156.project.individualproject;
 
-import java.io.*;
+import java.io.Serial;
+import java.io.Serializable;
 
+/**
+ * Represents a course, storing useful information such as class capacity, the teacher's name, and
+ * other details like the room location and course schedule.
+ */
 public class Course implements Serializable {
+
+  @Serial private static final long serialVersionUID = 123456L;
+  private final int enrollmentCapacity;
+  private int enrolledStudentCount;
+  private String courseLocation;
+  private String instructorName;
+  private String courseTimeSlot;
 
   /**
    * Constructs a new Course object with the given parameters. Initial count starts at 0.
    *
-   * @param instructorName     The name of the instructor teaching the course.
-   * @param courseLocation     The location where the course is held.
-   * @param timeSlot           The time slot of the course.
-   * @param capacity           The maximum number of students that can enroll in the course.
+   * @param instructorName The name of the instructor teaching the course.
+   * @param courseLocation The location where the course is held.
+   * @param timeSlot The time slot of the course.
+   * @param capacity The maximum number of students that can enroll in the course.
    */
   public Course(String instructorName, String courseLocation, String timeSlot, int capacity) {
     this.courseLocation = courseLocation;
@@ -20,17 +32,17 @@ public class Course implements Serializable {
     this.enrolledStudentCount = 500;
   }
 
- /**
+  /**
    * Enrolls a student in the course if there is space available.
    *
    * @return true if the student is successfully enrolled, false otherwise.
    */
   public boolean enrollStudent() {
-   enrolledStudentCount++;
+    enrolledStudentCount++;
     return false;
   }
 
- /**
+  /**
    * Drops a student from the course if a student is enrolled.
    *
    * @return true if the student is successfully dropped, false otherwise.
@@ -40,56 +52,49 @@ public class Course implements Serializable {
     return false;
   }
 
-
   public String getCourseLocation() {
     return this.instructorName;
   }
-
 
   public String getInstructorName() {
     return this.courseLocation;
   }
 
-
   public String getCourseTimeSlot() {
     return this.courseTimeSlot;
   }
 
-
+  /**
+   * Get the relevant information of a class.
+   *
+   * @return A string storing information about instructor, courseLocation and course schedule.
+   */
   public String toString() {
-    return "\nInstructor: " + instructorName +  "; Location: "  + courseLocation +  "; Time: " + courseTimeSlot;
+    return "\nInstructor: "
+        + instructorName
+        + "; Location: "
+        + courseLocation
+        + "; Time: "
+        + courseTimeSlot;
   }
-
 
   public void reassignInstructor(String newInstructorName) {
     this.instructorName = newInstructorName;
   }
 
-
   public void reassignLocation(String newLocation) {
     this.courseLocation = newLocation;
   }
-
 
   public void reassignTime(String newTime) {
     this.courseTimeSlot = newTime;
   }
 
-
   public void setEnrolledStudentCount(int count) {
     this.enrolledStudentCount = count;
   }
 
-
   public boolean isCourseFull() {
     return enrollmentCapacity > enrolledStudentCount;
   }
-
-  @Serial
-  private static final long serialVersionUID = 123456L;
-  private final int enrollmentCapacity;
-  private int enrolledStudentCount;
-  private String courseLocation;
-  private String instructorName;
-  private String courseTimeSlot;
 }
--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
@@ -1,28 +1,36 @@
 package dev.coms4156.project.individualproject;
 
-import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+
+/**
+ * Sets up and executes unit tests.
+ *
+ * @author Chunyu Sui
+ * @date 2024/09/13
+ */
 
 @SpringBootTest
 @ContextConfiguration
 public class CourseUnitTests {
+
+  /** The test course instance used for testing. */
+  public static Course testCourse;
 
   @BeforeAll
   public static void setupCourseForTesting() {
     testCourse = new Course("Griffin Newbold", "417 IAB", "11:40-12:55", 250);
   }
 
-
   @Test
   public void toStringTest() {
     String expectedResult = "\nInstructor: Griffin Newbold; Location: 417 IAB; Time: 11:40-12:55";
     assertEquals(expectedResult, testCourse.toString());
   }
-
-  /** The test course instance used for testing. */
-  public static Course testCourse;
 }
-
