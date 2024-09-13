541b098658d9d85e9eb1f14cf14c40c6a41b1550
Bryan Granda <bryan.a.granda@gmail.com>
2024-09-13 16:22:32 -0400

    test: added test methods for the functions in RouteController Class

--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/RouteControllerTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/RouteControllerTests.java
@@ -1,11 +1,15 @@
 package dev.coms4156.project.individualproject;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.HashMap;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
 
 /**
@@ -25,6 +29,102 @@ public class RouteControllerTests {
 
   @Test
   public void retrieveDepartmentTest() {
+    ResponseEntity<?> res = testRouteController.retrieveDepartment("COMS");
+    String body = res.getBody().toString();
+    HttpStatusCode status = res.getStatusCode();
+    assertEquals("COMS", body);
+    assertEquals(HttpStatus.OK, status);
+  }
+
+  @Test
+  public void retrieveCourseTest() {
+    ResponseEntity<?> res = testRouteController.retrieveCourse("COMS", 1004);
+    HttpStatusCode status = res.getStatusCode();
+    assertEquals(HttpStatus.OK, status);
+  }
+
+  @Test
+  public void isCourseFullTest() {
+    ResponseEntity<?> res = testRouteController.isCourseFull("COMS", 1004);
+    HttpStatusCode status = res.getStatusCode();
+    assertEquals(HttpStatus.OK, status);
+  }
+
+  @Test
+  public void getMajorCountFromDeptTest() {
+    ResponseEntity<?> res = testRouteController.getMajorCtFromDept("COMS");
+    HttpStatusCode status = res.getStatusCode();
+    assertEquals(HttpStatus.OK, status);
+  }
+
+  @Test
+  public void identifyDeptChairTest() {
+    ResponseEntity<?> res = testRouteController.identifyDeptChair("COMS");
+    HttpStatusCode status = res.getStatusCode();
+    assertEquals(HttpStatus.OK, status);
+  }
+
+  @Test
+  public void findCourseInstructorTest() {
+    ResponseEntity<?> res = testRouteController.findCourseInstructor("COMS", 1004);
+    HttpStatusCode status = res.getStatusCode();
+    assertEquals(HttpStatus.OK, status);
+  }
+
+  @Test
+  public void findCourseTimeTest() {
+    ResponseEntity<?> res = testRouteController.findCourseTime("COMS", 1004);
+    HttpStatusCode status = res.getStatusCode();
+    assertEquals(HttpStatus.OK, status);
+  }
+
+  @Test
+  public void addMajorToDeptTest() {
+    ResponseEntity<?> res = testRouteController.addMajorToDept("COMS");
+    HttpStatusCode status = res.getStatusCode();
+    assertEquals(HttpStatus.OK, status);
+  }
+
+  @Test
+  public void removeMajorFromDeptTest() {
+    ResponseEntity<?> res = testRouteController.removeMajorFromDept("CHEM");
+    HttpStatusCode status = res.getStatusCode();
+    assertEquals(HttpStatus.OK, status);
+  }
+
+  @Test
+  public void dropStudentTest() {
+    ResponseEntity<?> res = testRouteController.dropStudent("COMS", 1004);
+    HttpStatusCode status = res.getStatusCode();
+    assertEquals(HttpStatus.OK, status);
+  }
+
+  @Test
+  public void setEnrollmentCountTest() {
+    ResponseEntity<?> res = testRouteController.setEnrollmentCount("COMS", 1004, 300);
+    HttpStatusCode status = res.getStatusCode();
+    assertEquals(HttpStatus.OK, status);
+  }
+
+  @Test
+  public void changeCourseTimeTest() {
+    ResponseEntity<?> res = testRouteController.changeCourseTime("COMS", 1004, "10:10-11:25");
+    HttpStatusCode status = res.getStatusCode();
+    assertEquals(HttpStatus.OK, status);
+  }
+
+  @Test
+  public void changeCourseTeacherTest() {
+    ResponseEntity<?> res = testRouteController.changeCourseTeacher("CHEM", 1403, "Bryan Granda");
+    HttpStatusCode status = res.getStatusCode();
+    assertEquals(HttpStatus.OK, status);
+  }
+
+  @Test
+  public void changeCourseLocationTest() {
+    ResponseEntity<?> res = testRouteController.changeCourseLocation("CHEM", 1403, "401 URI");
+    HttpStatusCode status = res.getStatusCode();
+    assertEquals(HttpStatus.OK, status);
   }
 
 
