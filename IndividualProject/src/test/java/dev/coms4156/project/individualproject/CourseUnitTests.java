ca31491af39c8ab67f8c5f45398f764d77df0799
Jason Zou <tapjason@gmail.com>
2024-09-13 19:20:43 -0400

    Fixed random bugs, reached threshold coverage and finished

--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
@@ -1,7 +1,6 @@
 package dev.coms4156.project.individualproject;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
