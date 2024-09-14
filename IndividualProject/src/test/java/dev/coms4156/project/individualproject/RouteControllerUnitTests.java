e0b3da5d60f1bb10b0a1db10f1cd33d8df4ab256
CarrilloMario831 <carrillomario831@gmail.com>
2024-09-13 23:43:38 -0400

    I dont know why this test began failing. Quick fix.

--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/RouteControllerUnitTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/RouteControllerUnitTests.java
@@ -40,8 +40,8 @@ public class RouteControllerUnitTests {
   public void testIndex() {
     String response = routeController.index();
     String expected = "Welcome, in order to make an API call direct your browser or Postman "
-            + "to an endpoint \n\nThis can be done using the following format: \n\n"
-            + "http:127.0.0.1:8080/endpoint?arg=value";
+            + "to an endpoint \n\n This can be done using the following format: \n\n http:127.0.0"
+            + ".1:8080/endpoint?arg=value";
     assertEquals(expected, response);
   }
 
