68f6c1e40c58b1b1a6d1d28b58c57b36fe68d5c3
philluple <pnl2111@columbia.edu>
2024-09-13 22:24:51 -0400

    Fixed style for unit tests

--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/DepartmentUnitTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/DepartmentUnitTests.java
@@ -1,16 +1,16 @@
 package dev.coms4156.project.individualproject;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
 /**
- * Provides unit test for testing implementations in the Course class.
+ * Provides unit test for testing implementations in the Department class.
  */
 @SpringBootTest
 @ContextConfiguration
